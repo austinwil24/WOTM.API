d1be85ca27bb172c87c0fc6312a1d7685ef6a6f1
Austin Wilson <austinw@itworksglobal.com>
2017-03-01 15:31:54 -0500

    update nuget references

--- Api.WorkoutTimeMachine/src/Api.WorkoutTimeMachine.Controllers/V1/UserController.cs
+++ Api.WorkoutTimeMachine/src/Api.WorkoutTimeMachine.Controllers/V1/UserController.cs
@@ -1,10 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Api.WorkoutTimeMachine.Controllers.V1
 {
-    [Route("api/[controller]")]
-    public class Controller
+    [Route("api/v1/[controller]s")]
+    public class UserController : Controller
     {
+        private readonly ILogger _logger;
 
+        public UserController(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUser(string id)
+        {
+            _logger.LogDebug("{Class}.{Method}() Called.", nameof(UserController), nameof(GetUser));
+
+            return Ok(new { id });
+        }
     }
 }
